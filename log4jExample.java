3db2aeaf8834f551196d26ee43462f5775b876b9
Vikramsingh Kushwaha <kushwahavikramsingh193@gmail.com>
2019-12-28 22:14:32 +0530

    Add files via upload

---
+++ log4jExample.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class log4jExample{
+    /* Get actual class name to be printed on */
+    static Logger log = Logger.getLogger(log4jExample.class.getName());
+
+    public static void main(String[] args)throws IOException,SQLException{
+        log.debug("Debug");
+        log.info("Info");
+    }
+}
